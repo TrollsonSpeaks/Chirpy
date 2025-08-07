c4e9f9ae6be8ca697a806a66750ce112cde83866
TrollsonSpeaks <VictoriaSacino@gmail.com>
2025-08-06 22:12:38 -0700

    Serve Chirpy logo at /assets/logo.png

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ func main() {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("."))
-
 	mux.Handle("/", fileServer)
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	
 
 	server := &http.Server{
 		Addr:     ":8080",
