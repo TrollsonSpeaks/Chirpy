137f1edf72fbd6391fcfaa0e58d2c56c12a7d3e2
TrollsonSpeaks <VictoriaSacino@gmail.com>
2025-08-07 02:11:28 -0700

    Add /healthz readiness endpoint and move static iles to /app/

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	mux.HandleFunc("/healthz", readinessHandler)
 
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
-	
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))	
 
 	server := &http.Server{
 		Addr:     ":8080",
